747a03e73ae96a2a2868a03291b474c5a8fad27f
Hussein Fardous <hf2334@columbia.edu>
2020-06-20 19:10:21 -0400

    Added pymessenger Module and Created Echo Bot

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask, request, jsonify
+from pymessenger import Bot
 from wit import Wit
 
 app = Flask(__name__)
@@ -29,8 +30,10 @@ if not FB_APP_SECRET:
 
 #client = Wit(WIT_TOKEN)
 
+bot = Bot(FB_PAGE_TOKEN)
+
 # Webhook Setup
-@app.route("/", methods=["GET"])
+@app.route("/webhook", methods=["GET"])
 def verify():
     # When the endpoint is registered as a webhook, it must echo back the "hub.challenge" value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
@@ -40,7 +43,28 @@ def verify():
     return "Hello World!", 200
 
 # Message Handler
-@app.route("/", methods=["POST"])
+@app.route("/webhook", methods=["POST"])
 def webhook():
+    
     data = request.get_json()
-    print(data)
+
+    if data['object'] == 'page':
+        for entry in data['entry']:
+            for messaging_event in entry['messaging']:
+
+                # IDs
+                sender_id = messaging_event['sender']['id']
+                recipient_id = messaging_event['recipient']['id']
+
+                if messaging_event.get('message'):
+                    # Extracting text message
+                    if 'text' in messaging_event['message']:
+                        messaging_text = messaging_event['message']['text']
+                    else:
+                        messaging_text = 'no text'
+
+                    # Echo
+                    response = messaging_text
+                    bot.send_text_message(sender_id, response)
+
+    return "ok", 200
