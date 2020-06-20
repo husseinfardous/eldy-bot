5263a63c173ae1988a105c145bb4a1e4044ea043
Hussein Fardous <hf2334@columbia.edu>
2020-06-20 18:26:24 -0400

    Added wit Module and Print Received Message

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask, request, jsonify
-#from wit import Wit
+from wit import Wit
 
 app = Flask(__name__)
 
@@ -16,10 +16,10 @@ WIT_TOKEN = os.environ.get("WIT_TOKEN")
 # Messenger API Parameters
 FB_PAGE_TOKEN = os.environ.get("FB_PAGE_TOKEN")
 if not FB_PAGE_TOKEN:
-  raise ValueError("Missing FB_PAGE_TOKEN!")
+    raise ValueError("Missing FB PAGE TOKEN!")
 FB_APP_SECRET = os.environ.get("FB_APP_SECRET")
 if not FB_APP_SECRET:
-  raise ValueError("Missing FB_APP_SECRET!")
+    raise ValueError("Missing FB APP SECRET!")
 
 # ~~~~~~~~~~Messenger API~~~~~~~~~~
 
@@ -30,14 +30,17 @@ if not FB_APP_SECRET:
 #client = Wit(WIT_TOKEN)
 
 # Webhook Setup
-@app.route('/', methods=["GET"])
+@app.route("/", methods=["GET"])
 def verify():
-    # when the endpoint is registered as a webhook, it must echo back
-    # the 'hub.challenge' value it receives in the query arguments
-    print (request.headers)
+    # When the endpoint is registered as a webhook, it must echo back the "hub.challenge" value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == os.environ.get("FB_VERIFY_TOKEN"):
-            return "Verification token mismatch", 403
+            return "Verification Token Mismatch!", 403
         return request.args["hub.challenge"], 200
+    return "Hello World!", 200
 
-    return "Hello world", 200
+# Message Handler
+@app.route("/", methods=["POST"])
+def webhook():
+    data = request.get_json()
+    print(data)
