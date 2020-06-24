5b4464f336d258caaa4790e525f4fac36601b294
Janill Lema <jl4817@columbia.edu>
2020-06-24 05:29:46 -0400

    added airtable related functions

--- app.py
+++ app.py
@@ -106,11 +106,22 @@ general_coronavirus_info = None
 with open("general_coronavirus_info.json") as json_file:
     general_coronavirus_info = json.load(json_file)
 
+# Load US States and Abbreviations from JSON File 
+us_states_data = None
+with open("us_states.json") as json_file:
+    us_states_data = json.load(json_file)
+
 # Utilized when Location or Address isn't able to be Parsed
 prev_intent_name = None
 
 # Set of Intents within COVID-19 Statsitics Domain
 coronavirus_stats_intents = {"confirmed", "recovered", "deaths", "testsPerformed", "all_stats"}
+
+# Dictionary of US states map to an array of suppliers residing in that state
+supplier_state_dictionary = dict()
+
+# Timestamp of the last entry added to the Resource Provider Table 
+resource_providers_timestamp = '2020-05-22T03:36:27.000Z'
 
 geolocator = Nominatim(user_agent="Eldy Bot")
 
@@ -249,6 +260,138 @@ def val_to_str(val):
     if val is None:
         return "N/A"
     return str(val)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~ AirTable Functions ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+def check_new_entries_supplier_table():
+
+     global resource_providers_timestamp
+
+     created_time_string = "CREATED_TIME() > '" + resource_providers_timestamp
+     supplier_table = requests.get("https://api.airtable.com/v0/app62IQsAsxBquR8C/tbllMS68Zqkwm7nbn?fields%5B%5D=Pickup+Address&filterByFormula=" + created_time_string + "'&sort%5B0%5D%5Bfield%5D=created_time&sort%5B0%5D%5Bdirection%5D=asc&api_key=" + AIRTABLE_API_KEY , auth=HTTPBasicAuth(AIRTABLE_EMAIL, AIRTABLE_PASSWORD)).json()["records"]
+     
+     if (len(supplier_table) > 0):
+
+          resource_providers_timestamp = supplier_table[-1]['createdTime']
+
+          for k in supplier_table:
+
+               supplier_state = ""
+               supplier_address = k['fields'].get('Pickup Address')
+          
+               address_locator = geolocator.geocode(supplier_address, addressdetails=True)
+
+               if address_locator:
+                    supplier_state = address_locator.raw['address']['state']
+                    supplier_state_dictionary.setdefault(supplier_state,[]).append(k['id'])
+               else:
+                    supplier_address = ''.join([i for i in supplier_address if not i.isdigit()])
+                    parsed_address_array = [ x.strip().lstrip().lower() for x in supplier_address.split(",")]
+
+                    for idx in range(0, len(us_states_data["abbreviations"])):
+                         if us_states_data["abbreviations"][idx].lower() in parsed_address_array or us_states_data["states"][idx].lower() in parsed_address_array:
+                              supplier_state_dictionary.setdefault(us_states_data["states"][idx],[]).append(k['id'])
+                              break
+
+def find_supplies_intent(reciever_address, supplies_array): 
+
+     address_locator= geolocator.geocode(reciever_address, addressdetails=True)
+     if address_locator:
+          find_possible_resource_providers(address_locator.raw['address']['state'], reciever_address, supplies_array)
+     else:
+          handle_resource_request()
+
+
+ 
+def handle_resource_request(data=None):
+     if data == None:
+          return "I could not understand your address. Could you please write format your address as this sample address: 70 Morningside St., New York, NY 11207"
+     elif (len(data) == 0):
+          return "Sorry, I could not find any of the supplies requested near you."
+     else:
+          return supplier_information(data)
+
+def supplier_information(supplier_data):
+
+     response = ""
+     
+     for arr in supplier_data:
+
+          distance_away = str(arr[0])
+          supplier_name = arr[1]['fields']['Name']
+          response += supplier_name + " is " + distance_away + " miles away from you! \n" 
+
+          pick_up_address = arr[1]['fields']['Pickup Address']
+          response += "Pickup Address: " + pick_up_address +'\n'
+
+          items = arr[1]['fields'].get('Service/Items')
+          if items:
+               response += "Items/Services Available: " + ",".join(items)
+          
+          other_items = arr[1]['fields'].get('Other Items')
+          if other_items:
+               if ( items!= None ):
+                    response += "," + other_items  + "\n"
+               else:
+                    response += "Items/Services Available: " + other_items + "\n"
+          else:
+               response += "\n"
+          
+          phone_number = arr[1]['fields'].get('Phone Number')
+          if phone_number:
+               response += "Phone Number: " + phone_number + '\n'
+          
+          email = arr[1]['fields'].get('Email')
+          if email:
+               response += "Email: " + email + '\n'
+          
+          additional_notes = arr[1]['fields'].get('Additional Notes')
+          if additional_notes:
+               response += "Additional Notes: " + additional_notes + "\n"
+          
+          response += "\n"
+     return response
+
+          
+          
+          
+
+
+def find_possible_resource_providers(reciever_state, reciever_address, supply_array):
+     
+     possible_suppliers = ','.join(supplier_state_dictionary[reciever_state])   
+     possible_supplier_list = requests.get("https://api.airtable.com/v0/app62IQsAsxBquR8C/tbllMS68Zqkwm7nbn?filterByFormula=SEARCH(RECORD_ID()%2C+%27"+ possible_suppliers + "%27)+!%3D+%22%22&api_key=" + airtable_api_key, auth=HTTPBasicAuth(airtable_email, airtaible_password)).json()["records"]
+    
+     matching_suppliers_list = []
+     for supplier_dict in possible_supplier_list:
+          available_supplier_supplies = []
+          available_supplier_supplies = [s.lower() for s in supplier_dict.get('fields').get('Service/Items')] 
+          
+          other_items = supplier_dict.get('fields').get('Other Items')
+          if other_items:
+               other_items = other_items.split(",")
+               available_supplier_supplies += [s.lower() for s in other_items]
+          
+          if (any(item in available_supplier_supplies for item in supply_array)):
+               matching_suppliers_list.append(supplier_dict)
+     
+     find_providers_nearby(matching_suppliers_list, reciever_address)
+
+
+def find_providers_nearby(possible_suppliers, reciever_add):
+     providers_nearby = []
+
+     reciever_address = reciever_add.replace(" ", '+').replace(",", "%2C").replace('#','%')
+     for supplier in possible_suppliers:
+          supplier_address = supplier.get('fields').get("Pickup Address").replace(" ", '+').replace(",", "%2C").replace('#','%')
+          distance = requests.get("https://www.mapquestapi.com/directions/v2/alternateroutes?key=" + mapquest_api_key + "&from=" + reciever_address + "&to=" + supplier_address + "&outFormat=json&ambiguities=ignore&routeType=pedestrian&maxRoutes=1&timeOverage=0&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false&unit=M").json()['route']['distance']
+          if (distance <= 15):
+               providers_nearby.append([distance, supplier])
+     
+     handle_resource_request(sorted(providers_nearby))
+
+
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~Main Function~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
