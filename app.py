4f34383afa65791619b8af29461b9120361c4ed4
Hussein Fardous <hf2334@columbia.edu>
2020-06-26 01:50:41 -0400

    Fixed 'IndexError'

--- app.py
+++ app.py
@@ -329,23 +329,31 @@ def handle_interests(message_text):
             reply_message = "Here is your match!\n\n"
 
         for match in ids_to_overlapping_interests:
+            
             reply_message += companions_id_to_info[match]["Name"] + "\n"
+            
             reply_message += companions_id_to_info[match]["Pronouns"] + "\n"
+            
             for mode_of_contact in companions_id_to_info[match]["Preferred Mode of Contact"]:
                 reply_message += mode_of_contact + ", "
-            reply_message.rstrip()[len(reply_message) - 1]
-            reply_message += "\n"
+            reply_message = reply_message.rstrip()
+            reply_message = reply_message[len(reply_message) - 1] + "\n"
+            
             reply_message += companions_id_to_info[match]["Phone Number"] + "\n"
+            
             reply_message += companions_id_to_info[match]["Email"] + "\n"
+            
             reply_message += companions_id_to_info[match]["Additional Notes"] + "\n"
+            
             reply_message += "Common Interests/Hobbies: "
             for overlapping_interest in ids_to_overlapping_interests[match]:
                 reply_message += overlapping_interest + ", "
-            reply_message.rstrip()[len(reply_message) - 1]
-            reply_message += "\n\n"
+            reply_message = reply_message.rstrip()
+            reply_message = reply_message[len(reply_message) - 1] + "\n\n"
 
         ids_to_overlapping_interests = {}
-        return reply_message.rstrip()[len(reply_message) - 1]
+        reply_message = reply_message.rstrip()
+        return reply_message[len(reply_message) - 1]
 
     return reply_message
 
